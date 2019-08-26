538fdf7feb74a0c83cad2a27fbc5ce230cb798fa
Kurt Van Etten <kurt@vanetten.us>
2019-08-26 00:42:59 -0400

    MVP is done.  Add some dummy logic to the logicUnit for demonstration

--- src/logicUnit.js
+++ src/logicUnit.js
@@ -2,11 +2,27 @@
 
 const logicUnit = (keypress, currentFSM) => {
 
-	// let { currentValue, currentMemory, currentOperand, currentAppend } = currentFSM;
+	const { currentValue, currentMemory, currentOperand, currentAppend } = currentFSM;
 
-	const newValue = Math.floor(Math.random() * 10000);
+	let newValue = currentValue;
+	let newMemory = currentMemory;
+	let newOperand = currentOperand;
+	let newAppend = currentAppend;
 
-	const newFSM = {...currentFSM, value: newValue};
+
+	// we'll add some functionality later, but for now just set a random value
+	newValue = Math.floor(Math.random() * 10000);
+
+	// we can implement at least one key just to show this function works
+	if (keypress === 'clear') {
+		newValue = '0';
+		newMemory = null;
+		newOperand = null;
+		newAppend = true;
+	}
+
+
+	const newFSM = {value: newValue, memory: newMemory, operand: newOperand, append: newAppend};
 
 	return newFSM;
 };
