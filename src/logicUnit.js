5f99624bcc80d2874f2f1ab8fc764c3be37f9932
Kurt Van Etten <kurt@vanetten.us>
2019-08-26 20:36:39 -0400

    Add logicUnit.  Not fully functional yet

--- src/logicUnit.js
+++ src/logicUnit.js
@@ -2,24 +2,156 @@
 
 const logicUnit = (keypress, currentFSM) => {
 
-	const { currentValue, currentMemory, currentOperand, currentAppend } = currentFSM;
+	const maxlen = 14;  // maximum capacity of the display
 
-	let newValue = currentValue;
-	let newMemory = currentMemory;
-	let newOperand = currentOperand;
-	let newAppend = currentAppend;
+	// the 'state' of the calculator FSM:
+	const { value, memory, operand, append } = currentFSM;
+
+	let newValue = value;
+	let newMemory = memory;
+	let newOperand = operand;
+	let newAppend = append;
 
 
-	// we'll add some functionality later, but for now just set a random value
-	newValue = Math.floor(Math.random() * 10000).toString();
+	// the alphabet of our FSM consists of all the keys on the keypad
+	// based on the current state and the key pressed, determine the new state
+	switch (keypress) {
 
-	// we can implement at least one key just to show this function works
-	if (keypress === 'clear') {
-		newValue = '0';
-		newMemory = null;
-		newOperand = null;
-		newAppend = true;
-	}
+		// return everything to initial state
+		case('clear'):
+			newValue = '0';
+			newMemory = null;
+			newOperand = null;
+			newAppend = true;
+		break;
+
+
+		// change the sign ov the display value
+		case('sign'):
+			if (value !== '0') {
+				if (value.substring(0, 1) === '-') {
+					newValue = value.substring(1);
+				} else {
+					if (value.length < maxlen) {
+						newValue = '-' + value;
+					}
+				}
+			}
+		break;
+
+
+		case('percent'):
+			// TODO!
+			console.log('percent!');
+		break;
+
+
+		// if there is an operation pending, do that first
+		// save the just-entered operand and (possibly updated) display to memory
+		case('divide'):
+		case('times'):
+		case('minus'):
+		case('plus'):
+			newAppend = false;
+			if (operand != null) {
+				let valNum = Number.parseFloat(value);
+				let memNum = Number.parseFloat(memory);
+				let newNum = 0;
+				switch (operand) {
+					case('plus'):
+						newNum = memNum + valNum;
+					break;
+					case('minus'):
+						newNum = memNum - valNum;
+					break;
+					case('times'):
+						newNum = memNum * valNum;
+					break;
+					case('divide'):
+						newNum = memNum / valNum;
+					break;
+					default:
+					break;
+				}
+				newValue = newNum.toString();
+			}
+			newMemory = newValue;
+			newOperand = keypress;
+		break;
+
+
+		// perform any pending operations
+		// save the (possibly updated) display to memory
+		case('equals'):
+			newAppend = false;
+			if (operand != null) {
+				let valNum = Number.parseFloat(value);
+				let memNum = Number.parseFloat(memory);
+				let newNum = 0;
+				switch (operand) {
+					case('plus'):
+						newNum = memNum + valNum;
+					break;
+					case('minus'):
+						newNum = memNum - valNum;
+					break;
+					case('times'):
+						newNum = memNum * valNum;
+					break;
+					case('divide'):
+						newNum = memNum / valNum;
+					break;
+					default:
+					break;
+				}
+				newValue = newNum.toString();
+				newMemory = null;
+				newOperand = null;
+			}
+		break;
+
+
+		// handle radix point
+		case('point'):
+			if (append === false) {
+				newAppend = true;
+				newValue = '0.';
+			} else if (!value.includes('.') && value.length < maxlen) {
+				newValue = value + '.';
+			}
+		break;
+
+
+		// handle a digit
+		case('digit0'):
+		case('digit1'):
+		case('digit2'):
+		case('digit3'):
+		case('digit4'):
+		case('digit5'):
+		case('digit6'):
+		case('digit7'):
+		case('digit8'):
+		case('digit9'):
+			let newdigit = keypress.substring(5, 6);
+			if (append === false) {
+				newAppend = true;
+				newValue = newdigit;
+			} else if (value === '0') {
+				newValue = newdigit;
+			} else if (value.length < maxlen) {
+				newValue = value + newdigit;
+			}
+		break;
+
+
+		// oops! something we forgot to account for!
+		default:
+			console.log(`ERROR: no handler routine for "${keypress}"`);
+		break;
+
+	} // end of switch statement
+
 
 
 	const newFSM = {value: newValue, memory: newMemory, operand: newOperand, append: newAppend};
